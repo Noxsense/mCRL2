5fb01338596dda46eede5623f1b57e01d823ec5d
Hùòng Ngoc Lê <noxsense@gmail.com>
2020-02-03 00:49:30 +0100

    Implement LTSs to CS-Game, without weak branching
    - parse the cs-game nodes (challanges, answers)
    - pasre the possible moves (possible answers to challanges, etc.)
    - but without (contracted) weak transitions

--- libraries/lts/include/mcrl2/lts/lts_algorithm.h
+++ libraries/lts/include/mcrl2/lts/lts_algorithm.h
@@ -19,6 +19,17 @@
 #ifndef MCRL2_LTS_LTS_ALGORITHM_H
 #define MCRL2_LTS_LTS_ALGORITHM_H
 
+#include <algorithm>
+#include <cstdio>
+#include <cstdlib>
+// #include <functional>
+#include <iostream>
+#include <map>
+#include <stack>
+#include <string>
+#include <vector>
+#include "mcrl2/utilities/logger.h"
+#include "mcrl2/lts/lts.h"
 #include "mcrl2/lts/detail/liblts_bisim.h"
 #include "mcrl2/lts/detail/liblts_bisim_gjkw.h"
 #include "mcrl2/lts/detail/liblts_weak_bisim.h"
@@ -29,6 +40,7 @@
 #include "mcrl2/lts/lts_equivalence.h"
 #include "mcrl2/lts/lts_preorder.h"
 #include "mcrl2/lts/sigref.h"
+#include "mcrl2/lts/transition.h"
 
 namespace mcrl2
 {
@@ -42,6 +54,106 @@ namespace lts
  **/
 template <class LTS_TYPE>
 void reduce(LTS_TYPE& l, lts_equivalence eq);
+
+
+// TODO(nox): extrac this
+// NOX BA ---------------------------------------------------------------------
+
+const unsigned char NODE_ATK = 0;  // placeholder indicating node is an attacker node
+const unsigned char NODE_DEF = 1;  // placeholder indicating a node, which is alwas reachable as coupling.
+const unsigned char NODE_CPL = 2;  // placeholder indicating a node, which is alwas reachable as coupling.
+
+// std::vector<ACTION_LABEL_T> m_action_labels; // At position 0 we always find the label that corresponds to tau.
+// typedef ACTION_LABEL_T action_label_t;
+typedef struct _game_node
+{
+  unsigned char flag;
+  size_t act;
+  size_t p; size_t q;
+  bool swapped;  // false := p is from lts1
+} cs_game_node;
+
+typedef struct _game_move
+{
+  cs_game_node from, to;
+  size_t act;
+  // ? action_label;
+  bool weak;
+} move;
+
+bool operator==(const cs_game_node &n0, const cs_game_node &n1)
+{
+  return n0.flag == n1.flag
+    && n0.swapped == n1.swapped
+    && n0.act == n1.act
+    && n0.p == n1.p
+    && n0.q == n1.q;
+}
+bool operator!=(const cs_game_node &n0, const cs_game_node &n1)
+{
+  return !(n0 == n1);
+}
+
+bool operator<(const cs_game_node &n0, const cs_game_node &n1)
+{
+  return n0.flag != n1.flag ? n0.flag < n1.flag
+    : n0.act != n1.act ? n0.act < n1.act
+    : n0.swapped != n1.swapped ? n0.swapped < n1.swapped
+    : n0.p != n1.p ? n0.p < n1.p
+    : n0.q < n1.q;
+}
+
+std::string to_string(const cs_game_node &n)
+{
+  std::string fst = !n.swapped ? "p" : "q";
+  std::string snd = !n.swapped ? "q" : "p";
+
+  switch (n.flag)
+  {
+    case NODE_ATK:
+      return
+        "(" + fst + std::to_string(n.p) +
+        "," + snd + std::to_string(n.q) + ")a";
+
+    case NODE_CPL:
+      return "(Cpl,"
+        + fst + std::to_string(n.p) + ","
+        + snd + std::to_string(n.q) + ")d";
+
+    case NODE_DEF:
+      return
+        "(" + std::to_string(n.act) +
+        "," + fst + std::to_string(n.p) +
+        "," + snd + std::to_string(n.q) + ")d";
+
+    default: return "(<strange node>)?";
+  }
+}
+
+std::string to_string(const move &m)
+{
+  std::string tag0 = m.from.flag == NODE_ATK ? "<a>" : "<d>";
+  std::string tag1 = m.to.flag == NODE_ATK ? "<a>" : "<d>";
+
+  std::string alabel
+    = m.to.flag != NODE_DEF ? "" : " " + std::to_string(m.to.act);
+
+  return
+    "[" + tag0 + to_string(m.from) + "]"
+    + (alabel + (m.weak ? " => " : " -> "))
+    + "[" + tag1 + to_string(m.to) + "]";
+}
+
+bool equals(const move &m0, const move &m1, bool weak_transition = false)
+{
+  return m0.act == m1.act && (!weak_transition && !(m0.weak || m1.weak));
+}
+
+bool operator<(const move &m0, const move &m1)
+{
+  return m0.from != m1.from ? m0.from < m1.from : m0.to < m1.to;
+}
+// NOX BA ---------------------------------------------------------------------
 
 /** \brief Checks whether this LTS is equivalent to another LTS.
  * \param[in] l1 The first LTS that will be compared.
@@ -200,10 +312,333 @@ bool destructive_compare(LTS_TYPE& l1,
     }
     case lts_eq_coupled_sim:
     {
-      // TODO 2019-08-04 Bachelor Thesis
-      std::cout << "TODO 2019-08-04: This will check for coupled similarity.\n";
-      std::cout << "TODO 2019-08-11: How to read the graph.\n";
-      return true;
+      // USE TAU REDUCTION. =>  Reduces too much.
+      // TODO(nox) 2020-01-25 maybe bisimulation_reduce() instead?
+      if (false)
+      {
+        std::cout << "# TEST: ========== \n# Weak Bisim reduction and so on.\n";
+        // detail::tau_star_reduce(l1);
+        bool preserve_divergences = false;
+
+        detail::weak_bisimulation_reduce(l1,preserve_divergences);
+        detail::weak_bisimulation_reduce(l2,preserve_divergences);
+        // detail::tau_star_reduce(l2);
+        // /home/nox/Projects/collaboration/mCRL2/libraries/lts/include/mcrl2/lts/detail/liblts_tau_star_reduce.h
+
+        std::string DEBUG_NOTE = "'";
+        {
+          std::cout << "# TEST: l1.get_transitions():\n";
+          for (transition t : l1.get_transitions())
+          {
+            std::cout
+              << "[p" << DEBUG_NOTE << ": " << t.from() << "]"
+              << " " << (t.label()) << "="
+              // << (l1.is_tau(t.label()) ? "\u03c4" : std::to_string(l1.action_label(t.label())))
+              << (l1.action_label(t.label()))
+              << " ->"
+              << " [p" << DEBUG_NOTE << ": " << t.to() << "]\n";
+          }
+
+          std::cout << "# TEST: l2.get_transitions():\n";
+          for (const transition t : l2.get_transitions())
+          {
+            std::cout
+              << "[q" << DEBUG_NOTE << ": " << t.from() << "]"
+              << " " << (t.label()) << "="
+              // << (l2.is_tau(t.label()) ? "\u03c4" : std::to_string(l2.action_label(t.label())))
+              << (l2.action_label(t.label()))
+              << " ->"
+              << " [q" << DEBUG_NOTE << ": " << t.to() << "]\n";
+          }
+        }
+      }
+
+      std::cout << "# Attacker nodes (p,q)a in Ga ... as S1 x S2 aka all possible pairs between them\n";
+      std::cout << "# Prepare defender nodes 1: all possible nodes, and how they are reached.\n";
+
+      std::set<cs_game_node> attacker_nodes;  // (flag=NODE_ATK, placeholder, node::int, node::int)
+      std::set<cs_game_node> defender_nodes;  // (flag, act::int, (node:int, node::int))
+
+      // std::vector<move> moves;  // moves (node,node)
+      std::set<move> moves;  // moves (node,node)
+
+      /* Define game nodes here. */
+
+      /* Get Weak transitions. */
+      // NOP
+
+      /* filter transitions of t2. */
+      std::map<size_t,std::vector<transition>>
+        t2_tran_from_node, t2_tran_into_node,
+        t1_tran_from_node, t1_tran_into_node;
+
+      for (const transition t2 : l2.get_transitions())
+      {
+        t2_tran_from_node[t2.from()].push_back(t2);  // outgoing
+        t2_tran_into_node[t2.to()].push_back(t2);  // incoming
+
+        /* If tau, then add to weak transitions. */
+        if (l2.is_tau(t2.label()))
+        {
+          // NOP
+        }
+
+        // DEBUG
+          std::cout << "[q" << t2.from() << "]"
+            << " " << (l2.action_label(t2.label()))
+            << " ->"
+            << " [q" << t2.to() << "]\n";
+      }
+
+      /* Add weak transititions. */
+      // NOP
+
+      /* Create Attacker nodes (P,Q),
+       * .. similarity game defender nodes (A1,P,Q) -> (P,Q)
+       * .. and answering swapped similarity challanges (B,Q,P) => (Q,P)
+       */
+      for (transition t1 : l1.get_transitions())
+      {
+        size_t a, p0, p1;
+        // ? act_label = t1.action_label(act);
+
+        a = t1.label();
+        p0 = t1.from();
+        p1 = t1.to();
+
+        // bool a_is_tau = l1.is_tau(a);
+
+        // for reusing.
+        t1_tran_from_node[p0].push_back(t1);  // outgoing
+        t1_tran_into_node[p1].push_back(t1);  // incoming
+
+        // DEBUG
+          std::cout
+            << "[p" << t1.from() << "]"
+            << " " << (l1.action_label(t1.label()))
+            << " ->"
+            << " [p" << t1.to() << "]\n";
+
+        /* Translate transition (p0) a -> (p1) to game nodes and moves:
+         * (p0,q) a -> (a,p1,q) for all q in t2.num_states()
+         * Create also (p0,q) -> (cpl, p0, q) */
+
+        for (size_t q = 0; q < l2.num_states(); q++)
+        {
+          /* Challange: (Weak) Simulation Game. */
+          cs_game_node node_atk = {NODE_ATK, 0, p0, q, false}; // (p0,q) f.a q in L2
+          cs_game_node node_sim = {NODE_DEF, a, p1, q, false}; // (a,p1,q) f.a q in L2
+
+          attacker_nodes.insert(node_atk);
+          defender_nodes.insert(node_sim);
+
+          /* p0 -> p1 */
+          // moves.push_back({node_atk, node_sim, a, false});
+          moves.insert({node_atk, node_sim, a, false});
+
+          /* Answer to swapped challange.
+           * (b,q,p0) -> (q,p1), if p0 b=> p1. */
+          for (transition bqq1 : t2_tran_into_node[q])
+          {
+            size_t b = bqq1.label();
+            bool b_is_tau = l2.is_tau(b);
+            // if (b_is_tau || l2.action_label(b) == l1.action_label(a))
+            if (l2.action_label(b) == l1.action_label(a))
+            {
+              // moves.push_back({
+              moves.insert({
+                  {NODE_DEF, b, q, p0, true},
+                  {NODE_ATK, 0, q, p1, true},
+                  a,
+                  false});  // one weak step is no weak transition.
+            }
+          }
+        }
+      }
+
+      /* Create (swapped) Attacker nodes (Q,P),
+       * .. (swapped) similarity game defender nodes (B,Q,P) -> (Q,P)
+       * .. and answering similarity challanges (A,P,Q) => (P,Q)
+       */
+      for (transition t2 : l2.get_transitions())
+      {
+        size_t b, q0, q1;
+        // ? act_label = t1.action_label(act);
+
+        b = t2.label();
+        q0 = t2.from();
+        q1 = t2.to();
+
+        // bool b_is_tau = l2.is_tau(b);
+
+        /* Translate transition (p0) a -> (p1) to game nodes and moves:
+         * (p0,q) a -> (a,p1,q) for all q in t2.num_states()
+         * Create also (p0,q) -> (cpl, p0, q) */
+
+        for (size_t p = 0; p < l1.num_states(); p++)
+        {
+          /* Challange: Swapped (Weak) Simulation Game. */
+          // XXX
+          cs_game_node node_atk = {NODE_ATK, 0, q0, p, true};
+          cs_game_node node_sim = {NODE_DEF, b, q1, p, true};
+
+          attacker_nodes.insert(node_atk);
+          defender_nodes.insert(node_sim);
+
+          /* q0 -> q1 (swapped) .*/
+          // moves.push_back({node_atk, node_sim, b, false});
+          moves.insert({node_atk, node_sim, b, false});
+
+          /* Answer to challenge.
+           * (a,p,q0) -> (p,q1), if q0 a=> q1. */
+          for (transition bpp1 : t1_tran_into_node[p])
+          {
+            size_t a = bpp1.label();
+            bool a_is_tau = false && l2.is_tau(b);
+            if (a_is_tau || l2.action_label(b) == l1.action_label(a))
+            {
+              // moves.push_back({
+              moves.insert({
+                  {NODE_DEF, a, p, q0, false},
+                  {NODE_ATK, 0, p, q1, false},
+                  b,
+                  false});  // one weak step is no weak transition.
+            }
+          }
+        }
+      }
+
+      /* Add all Coupling challanges and their answers. */
+      for (size_t p = 0; p < l1.num_states(); p++)
+      {
+        for (size_t q = 0; q < l2.num_states(); q++)
+        {
+          /* Challange coupling */
+          cs_game_node node_atk = {NODE_ATK, 0, p, q, false};
+          // cs_game_node node_cpl = {NODE_CPL, 0, p, q, false};
+          cs_game_node node_swp = {NODE_ATK, 0, q, p, true};
+
+          attacker_nodes.insert(node_atk);
+          attacker_nodes.insert(node_swp);
+
+          // defender_nodes.insert(node_cpl);
+
+          // moves.push_back({node_atk, node_cpl, 0, false});
+          // moves.push_back({node_cpl, node_swp, 0, false});
+
+          // bisim.
+          moves.insert({node_atk, node_swp, 0, false});
+          moves.insert({node_swp, node_atk, 0, false});
+        }
+      }
+
+      // DEBUG
+      std::cout << "# Now, a Game with "
+        << attacker_nodes.size() << " Attacker nodes, "
+        << defender_nodes.size() << " Defender nodes and "
+        << moves.size() << " (unready) moves exists\n";
+
+      std::cout << "# Get all the predecessors.\n";
+      std::cout << "# Count their successors\n";
+      std::cout << "# Mark everyone won by defender (d)\n";
+
+      std::map<cs_game_node,std::set<cs_game_node>> predecessors;
+      std::map<cs_game_node,int> successor_count;
+      std::map<cs_game_node,int> nodes_won;
+      const int WON_DEFENDER = 0, WON_ATTACKER = 1;
+
+      std::cout // XXX REMOVE_ME
+        << "\n#title: ltscompare_coupledsim_csgame"
+        << "\n#fontSize: 10"
+        << "\n#arrowSize: 0.2"
+        << "\n#lineWidth: 1.0"
+        << "\n#zoom: 1.0"
+        << "\n#edges: rounded"
+        << "\n#.a: fill=#f77"
+        << "\n#.d: fill=#7f7 visual=roundrect"
+        << "\n#.l: visual=none"
+        << "\n#ranker: longest-path"
+        << "\n";
+
+      for (const auto &m : moves)
+      {
+        cs_game_node pred = m.from;
+        cs_game_node succ = m.to;
+
+        /* All nodes set won by defender. */
+        nodes_won[pred] = WON_DEFENDER;
+        nodes_won[succ] = WON_DEFENDER;
+
+        /* Update predecessors for succ.
+         * Predecessors[succ] += [pred] */
+        predecessors[succ].insert(pred);  // append predecessors.
+
+        /* Update succesors for the pred. */
+        successor_count[pred] += 1;  // "append" successors.
+
+        // DEBUG
+        std::cout << to_string(m) << "\n";
+      }
+
+      std::cout << "\n# Run: Computing Winning Regions.\n";
+
+      std::stack<cs_game_node> todo;
+      for (cs_game_node d : defender_nodes) todo.push(d); // XXX make me better
+      // todo.assign(defender_nodes.begin(), defender_nodes.end());
+
+      while (!todo.empty())
+      {
+        /* Pop from queue. */
+        cs_game_node n = todo.top();
+        todo.pop();
+
+        if (successor_count[n] <= 0)
+        {
+          std::cout << "// propagate_attacker_win(" << to_string(n) << ")\n";
+          if (nodes_won[n] == WON_DEFENDER)
+          {
+            nodes_won[n] = WON_ATTACKER;
+
+            /* now reduce it from all predecessors as successor.
+             * and check if the predecessor is also about to be won by the
+             * attacker. */
+            for (cs_game_node pred : predecessors[n])
+            {
+              successor_count[pred] -= 1;
+              if (successor_count[pred] < 1 || attacker_nodes.count(pred))
+              {
+                todo.push(pred);
+                successor_count[pred] = 0; // to propagate next run.
+              }
+            }
+          }
+        }
+      }
+
+      std::cout << "> R = {";
+      char seperator[3] = {'\0', ' ', '\0'};
+
+      // Filter attacker nodes, which won the challange.
+      std::set<cs_game_node> cs_relation;
+
+      for (const auto &pq : attacker_nodes)
+      {
+        if (nodes_won[pq] == WON_DEFENDER)
+        {
+          cs_relation.insert(pq);
+          std::cout << seperator << to_string(pq);
+          seperator[0] = ',';
+        }
+      }
+      std::cout << "}\n";
+
+      /* Return true iff root nodes are in R / won by defender. */
+      cs_game_node roots[]
+        = {{NODE_ATK, 0, 0, 0, false}, {NODE_ATK, 0, 0, 0, true}};
+
+      return
+        nodes_won[roots[0]] == WON_DEFENDER &&
+        nodes_won[roots[1]] == WON_DEFENDER;
     }
     default:
     throw mcrl2::runtime_error("Comparison for this equivalence is not available");
